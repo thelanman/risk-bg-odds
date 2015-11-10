29e36101b7b591ade29c2bab40f426a7a7ffd49a
thelanman <lanahan.j@gmail.com>
2015-11-09 21:27:30 -0500

    added support for multiple consecutive battles

--- odds.py
+++ odds.py
@@ -45,6 +45,27 @@ def do_combat(a, d):
     return a, d
 
 
+def multi_odds(a, d, n=100000):
+    a_wins = 0
+    a_left_sum = 0
+    d_left_sum = 0
+    a_left = a
+    for i in range(n):
+        a_left = a
+        for mi, m in enumerate(d):
+            a_left, d_left = do_combat(a_left, m)
+            if a_left == 1 or (mi + 1) == len(d):
+                #print a_left, mi, d_left
+                a_left_sum += a_left
+                d_left_sum += d_left + (sum(d[mi + 1:]) if (mi + 1) < len(d) else 0)
+                if (mi + 1) == len(d) and d_left == 0:
+                    a_wins += 1
+                break
+            a_left -= 1
+    return a_wins / float(n), a_left_sum / float(n), d_left_sum / float(n)
+
+
+
 def odds(a, d, n=100000):
     a_wins = 0
     a_left_sum = 0
@@ -61,11 +82,17 @@ def odds(a, d, n=100000):
 if __name__ == '__main__':
     import sys
     if len(sys.argv) < 3:
-        print 'USAGE: python odds.py attacker_armies defender_armies'
+        print 'USAGE: python odds.py attacker_armies defender_army[ies]'
         print 'EXAMPLE: python odds.py 15 11'
+        print '       : python odds.py 15 7 3 1'
         sys.exit()
-    a, d = sys.argv[1:]
+    a = sys.argv[1]
+    d = sys.argv[2:]
     a = int(a)
-    d = int(d)
-    o, al, dl = odds(a, d)
+    d = [int(x) for x in d]
+    o, al, dl = 0.0, 0.0, 0.0
+    if len(d) == 1:
+        o, al, dl = odds(a, d[0])
+    else:
+        o, al, dl = multi_odds(a, d)
     print 'Odds: %2.1f%%  Attacker Rem: %2.1f  Defender Rem: %2.1f' % (o * 100, al, dl)
